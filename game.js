3523c508dd16815346d9acd736fcdef9f4eb45f6
Rehab <88044583+rehabnaf@users.noreply.github.com>
2024-08-22 15:57:30 +0530

    Add the playRound function to allow the user to play a single round

--- game.js
+++ game.js
@@ -20,3 +20,40 @@ function getHumanChoice() {
 }
 let humanScore = 0;
 let computerScore = 0;
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == computerChoice) { // 3 combinations are handled with this conditional statement
+        console.log("It's a draw.")
+    }
+    if (humanChoice != computerChoice) {
+        if (humanChoice == "rock" && computerChoice == "scissors") {
+            console.log("You won! Rock beats Scissors.")
+            ++humanScore;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
+            console.log("You lose! Rock beats Scissors.")
+            ++computerScore;
+        }
+        else if (humanChoice == "paper" && computerChoice == "rock") {
+            console.log("You won! Paper beats Rock.")
+            ++humanScore;
+        }
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+            console.log("You lose! Paper beats Rock.")
+            ++computerScore;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
+            console.log("You won! Scissors beats Paper.")
+            ++humanScore;
+        }
+        else {
+            console.log("You lose! Scissors beats Paper.")
+            ++computerScore;
+        }
+    }
+}
+playRound(humanChoice, computerChoice);
